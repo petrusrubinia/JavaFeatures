909b804dbc656238ef7583dcfe65af5c0d21c325
petrusrubinia <petrusrubinia@yahoo.com>
2022-05-25 14:24:13 +0300

    replace the creation of predicate with lambda expression

--- src/com/company/consumerSupplier/PredicateDemo.java
+++ src/com/company/consumerSupplier/PredicateDemo.java
@@ -5,14 +5,11 @@ import java.util.function.Predicate;
 public class PredicateDemo {
 
     public static void main(String[] args) {
-        Predicate<Integer> predicate = new Predicate<Integer>() {
-            @Override
-            public boolean test(Integer integer) {
-                if (integer % 2 == 0) {
-                    return true;
-                }
-                return false;
+        Predicate<Integer> predicate = integer -> {
+            if (integer % 2 == 0) {
+                return true;
             }
+            return false;
         };
         System.out.println(predicate.test(7));
     }
